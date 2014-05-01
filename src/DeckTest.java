3b4b3df991c663ac51c4c533140d71c4d7a0d1b0
lajnus <Linus@Linus-Wareus-MacBook-Pro.local>
2014-05-01 10:52:34 +0200

    DeckTest

--- src/DeckTest.java
+++ src/DeckTest.java
@@ -1,14 +1,50 @@
 import static org.junit.Assert.*;
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import java.util.EmptyStackException;
 
-
+/**
+ * The test class DeckTest.
+ * This is the test class for the class Deck.
+ * 
+ * @author Linus Wåreus
+ * @version 2014.05.01
+ */
 public class DeckTest {
+	private Deck deck;
+	/**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+	@Before
+	public void setUp() throws Exception {
+		deck = new Deck();
+	}
 
-	setUp() {
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	/**
+	 * Tests the drawCard method.
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDrawCard() {
+		while (true) {
+			try {
+				assertNotNull(deck.drawCard());
+			} catch (EmptyStackException e) {
+				break;
+			}
+		}
 	}
 
 }
